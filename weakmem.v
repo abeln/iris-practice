4ef1eef3a51bdc95992e422bcd19be21b682bb11
Abel Nieto <abeln@cs.au.dk>
2020-03-31 15:57:34 +0200

    Done except for namespaces

--- weakmem.v
+++ weakmem.v
@@ -25,7 +25,7 @@ Section mp_model.
     (l_in ↦ #37 ∨ own γ (Excl ()))%I.
   
   Definition inv_out (l_out l_in : loc) (γ : gname) : iProp :=
-    inv N_out (l_out ↦ #0 ∨ l_out ↦ #1 ∗ inv N_in (inv_in l_in γ))%I.   
+    (l_out ↦ #0 ∨ l_out ↦ #1 ∗ inv N_in (inv_in l_in γ))%I.   
 End mp_model.
 
 Section mp_code.
@@ -35,7 +35,7 @@ Section mp_code.
   Definition repeat_prog : val :=
     rec: "repeat" "l" :=
       let: "vl" := !"l" in
-      if: "vl" = #0 then "repeat" "l" else "vl".
+      if: "vl" = #0 then ("repeat" "l") else "vl".
 
   (* Then we have the code for the example. *)
   Definition mp : val :=
@@ -50,7 +50,50 @@ End mp_code.
 Section mp_spec.
   
   Lemma mp_spec :
-    {{{ True }}} repeat_prog #() {{{ v, RET #v ; ⌜v = 37⌝ }}}.
+    {{{ True }}} mp #() {{{ v, RET #v ; ⌜v = 37⌝ }}}.
+  Proof.
+    iIntros (Φ) "_ HPost".
+    rewrite /mp. wp_pures.
+    wp_alloc lx as "Hlx". wp_pures. wp_alloc ly as "Hly". wp_pures.
+    wp_bind (par _ _).
+    iMod (own_alloc (Excl ())) as (γ) "Hown".
+    { constructor. }    
+    iMod (inv_alloc N_out _ (inv_out ly lx γ) with "[Hly]") as "#Hinv".
+    { iNext. rewrite /inv_out. iLeft. iFrame. }
+    wp_apply (wp_par (λ _, True)%I (λ vx, ⌜vx = #37⌝)%I with "[Hinv Hlx] [Hinv Hown]").
+    - wp_store.
+      iMod (inv_alloc N_in _ (inv_in lx γ) with "[Hlx]") as "#Hinv_in".
+      { iNext. iLeft. iFrame. }
+      iInv N_out as "[Hy0 | [Hy1 _]]" "Hclose"; wp_store.
+      * iMod ("Hclose" with "[Hinv_in Hy0]") as "_".        
+        iNext; iRight; iFrame; iFrame "#". by iModIntro.
+      * iMod ("Hclose" with "[Hinv_in Hy1]") as "_".        
+        iNext; iRight; iFrame; iFrame "#". by iModIntro.
+    - wp_bind (repeat_prog _).
+      iLöb as "IH".
+      rewrite /repeat_prog. wp_pures. wp_bind (!_)%E.
+      iInv N_out as "[Hy0 | [Hy1 #Hinv_in]]" "Hclose".
+      * wp_load.
+        iMod ("Hclose" with "[Hy0]") as "_".
+        { iLeft. iNext. iFrame. }
+        iModIntro.
+        wp_let. wp_pure _. wp_if.
+        iApply "IH". iFrame.
+      * wp_load.
+        iInv N_in as "[Hlx | Hown2]" "Hclose_in".
+        { admit. }
+        + iMod ("Hclose_in" with "[Hown]") as "_".
+          { iRight. iNext; iFrame. }
+          iMod ("Hclose" with "[Hy1]") as "_".
+          { iRight. iNext. by iFrame; iFrame "#". }
+          iModIntro. wp_pures. wp_load. iPureIntro; auto.
+        +  iDestruct "Hown2" as "> Hown2". iExFalso.
+           iDestruct (own_valid_2 with "Hown Hown2") as %Hv. done.
+    - iIntros (v1 v2) "[_ ->]".
+      iNext. wp_pures. iApply "HPost". iPureIntro. reflexivity.
+  Qed.
+      
+           
   
   (* Global Opaque newlock release acquire. *)
 End lock_spec.
