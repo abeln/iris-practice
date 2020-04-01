75a7189d359abfc1df9979f1c8d524123a88e54c
Abel Nieto <abeln@cs.au.dk>
2020-04-01 12:13:09 +0200

    Fix namespace issue. Proof is complete.

--- weakmem.v
+++ weakmem.v
@@ -18,16 +18,18 @@ From iris.heap_lang.lib Require Import par.
 Context `{heapG Σ}.
 Context `{inG Σ (exclR unitR)}.
 Context (N_out N_in : namespace).
+Context `{!spawnG Σ}.
+
 Notation iProp := (iProp Σ).
 
 Section mp_model.
-  Definition invN (l : loc) := nroot .@ "inv" .@ l.  
+  Definition invN (name : string) := nroot .@ "inv" .@ name.  
   
   Definition inv_in (l_in : loc) (γ : gname) : iProp :=
     (l_in ↦ #37 ∨ own γ (Excl ()))%I.
   
   Definition inv_out (l_out l_in : loc) (γ : gname) : iProp :=
-    (l_out ↦ #0 ∨ l_out ↦ #1 ∗ inv (invN l_in) (inv_in l_in γ))%I.
+    (l_out ↦ #0 ∨ l_out ↦ #1 ∗ inv (invN "inner") (inv_in l_in γ))%I.
 End mp_model.
 
 Section mp_code.
@@ -60,13 +62,13 @@ Section mp_spec.
     wp_bind (par _ _).
     iMod (own_alloc (Excl ())) as (γ) "Hown".
     { constructor. }    
-    iMod (inv_alloc (invN ly) _ (inv_out ly lx γ) with "[Hly]") as "#Hinv".
+    iMod (inv_alloc (invN "outer") _ (inv_out ly lx γ) with "[Hly]") as "#Hinv".
     { iNext. rewrite /inv_out. iLeft. iFrame. }
     wp_apply (wp_par (λ _, True)%I (λ vx, ⌜vx = #37⌝)%I with "[Hinv Hlx] [Hinv Hown]").
     - wp_store.
-      iMod (inv_alloc (invN lx) _ (inv_in lx γ) with "[Hlx]") as "#Hinv_in".
+      iMod (inv_alloc (invN "inner") _ (inv_in lx γ) with "[Hlx]") as "#Hinv_in".
       { iNext. iLeft. iFrame. }
-      iInv (invN ly) as "[Hy0 | [Hy1 _]]" "Hclose"; wp_store.
+      iInv (invN "outer") as "[Hy0 | [Hy1 _]]" "Hclose"; wp_store.
       * iMod ("Hclose" with "[Hinv_in Hy0]") as "_".        
         iNext; iRight; iFrame; iFrame "#". by iModIntro.
       * iMod ("Hclose" with "[Hinv_in Hy1]") as "_".        
@@ -74,7 +76,7 @@ Section mp_spec.
     - wp_bind (repeat_prog _).
       iLöb as "IH".
       rewrite /repeat_prog. wp_pures. wp_bind (!_)%E.
-      iInv (invN ly) as "[Hy0 | [Hy1 #Hinv_in]]" "Hclose".
+      iInv (invN "outer") as "[Hy0 | [Hy1 #Hinv_in]]" "Hclose".
       * wp_load.
         iMod ("Hclose" with "[Hy0]") as "_".
         { iLeft. iNext. iFrame. }
@@ -82,8 +84,7 @@ Section mp_spec.
         wp_let. wp_pure _. wp_if.
         iApply "IH". iFrame.
       * wp_load.
-        iInv (invN lx) as "[Hlx | Hown2]" "Hclose_in".
-        { rewrite /invN.  }
+        iInv (invN "inner") as "[Hlx | Hown2]" "Hclose_in".        
         + iMod ("Hclose_in" with "[Hown]") as "_".
           { iRight. iNext; iFrame. }
           iMod ("Hclose" with "[Hy1]") as "_".
@@ -98,10 +99,6 @@ Section mp_spec.
            
   
   (* Global Opaque newlock release acquire. *)
-End lock_spec.
+End mp_spec.
 
 
-Typeclasses Opaque locked.
-Global Opaque locked.
-Typeclasses Opaque is_lock.
-Global Opaque is_lock.
