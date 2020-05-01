46171d367cd0be3e10c0186d340e8f981448474d
Abel Nieto <abeln@cs.au.dk>
2020-05-01 16:09:43 +0200

    Add new file

--- slwm/abstract_ops.v
+++ slwm/abstract_ops.v
@@ -9,8 +9,15 @@ This file: abstract bag specification
 *)
 From iris.heap_lang Require Export lifting notation.
 From iris.base_logic.lib Require Export invariants.
-From stdpp Require Import gmultiset.
-Set Default Proof Using "Type".
+From iris.program_logic Require Export weakestpre.
+From iris.base_logic.lib Require Export invariants.
+From iris.proofmode Require Import tactics.
+From iris.heap_lang Require Import proofmode.
+From iris.heap_lang Require Import notation lang.
+From iris.heap_lang.lib Require Import par.
+From iris.bi Require Export notation.
+
+
 
 Record ops Σ `{!heapG Σ} :=
   Ops {
@@ -18,10 +25,91 @@ Record ops Σ `{!heapG Σ} :=
       read : val;
 
       (* specs *)
-      read_spec (l : loc) :
-        {{{ True }}} read #l {{{ v, RET #v; True }}};
+      read_spec (l : loc) (m : val) (q : Qp) (E : coPset) (N : namespace) : 
+        (True ={E, E ∖ ↑N}=> (▷ l ↦{q} m) ∗ (▷ l ↦{q} m -∗ |={E ∖ ↑N, E}=> True)) -∗
+        {{{ True }}} read #l @E {{{RET m; True}}};
+    }.
 
-  (*                                
+
+Arguments read {_ _} _.
+Arguments read_spec {_ _} _ _ _ _.
+
+
+Section impl.
+  Context Σ `{!heapG Σ}.
+
+  Definition read_impl : val :=
+    λ: "l", !"l".
+
+  Lemma read_impl_spec (l : loc) (m : val) (q : Qp) (E : coPset) (N : namespace) :
+    (True ={E, E ∖ ↑N}=> (▷ l ↦{q} m) ∗ (▷ l ↦{q} m -∗ |={E ∖ ↑N, E}=> True)) -∗
+    {{{ True }}} read_impl #l @E {{{RET m; True}}}.
+  Proof.
+    iIntros "#Hvs" (ϕ).
+    iModIntro. iIntros "_ Hcont".
+    rewrite /read_impl. wp_pures.
+    iMod ("Hvs" with "[]") as "[Hl Hclose]"; auto.
+    wp_load. iMod ("Hclose" with "Hl") as "_".
+    iModIntro. by iApply "Hcont".
+  Qed.
+
+  (*
+  iModIntro.
+  Qed.
+  *)
+
+  Definition ops_impl :=
+    {|
+      read := read_impl;
+      read_spec := read_impl_spec;
+    |}.    
+  
+End impl.
+
+Section client.
+  Context Σ `{!heapG Σ}.
+  
+  Variable O : ops Σ.
+  Variable N : namespace.
+
+  Lemma read_seq_spec (l : loc) (w : val) :
+    {{{ l ↦ w }}} O.(read) #l {{{ RET w; l ↦ w }}}.
+  Proof.
+    iIntros (ϕ) "Hpre Hcont".
+    wp_apply ((O.(read_spec) l w 1%Qp (l ↦ w)%I True N) with "[] [Hpre] [Hcont]"); auto.
+    iModIntro. iIntros "[_ Hl]". iApply "Hcont". iFrame.
+  Qed.
+  
+  (* Demonstrate logical atomicity *)
+  Definition par_read : val :=
+    λ: <>,
+       let: "l" := ref #0 in
+       O.(read) "l";;
+       (O.(read) "l") ||| (O.(read) "l").
+
+  Lemma par_read_spec :
+    {{{ True }}} par_read #() {{{ RET (#0, #0); True }}}.
+  Proof.
+    iIntros (ϕ) "_ Hcont".
+    rewrite /par_read. wp_pures.
+    wp_alloc l as "Hl". wp_pures.
+    (* Use the sequential spec *)
+    wp_apply (read_seq_spec with "Hl").
+    iIntros "Hl". wp_pures.
+    iMod (inv_alloc N _ (l ↦ #0) with "Hl") as "#Hinv".
+    wp_apply (wp_par (λ v, ⌜v = #0⌝)%I (λ v, ⌜v = #0⌝)%I).
+    - wp_apply (O.(read_spec) l #0 1%Qp True True N); auto.
+      iModIntro. iIntros "_".
+      iInv N as "Hinv2".
+      iPoseProof (O.(read_spec) l #0 1%Qp True True N) as "Hspec".
+      iApply "Hspec"; auto.
+      
+      
+    
+  
+End client.
+
+ (*                                
   (* -- operations -- *)
   newBag : val;
   pushBag : val;
@@ -56,27 +144,6 @@ Record ops Σ `{!heapG Σ} :=
       popBag b
     {{{ v, RET v; Q v }}};
   *)
-    }.
-
-Section impl.
-  Context Σ `{!heapG Σ}.
-
-  Definition read_impl : val :=
-    λ: "l", !"l".
-
-  Lemma read_impl_spec (l : loc) :
-    {{{ True }}} read_impl #l {{{ v, RET #v; True }}}.
-  Proof.
-  Admitted.
-
-  Definition ops_impl :=
-    {|
-      read := read_impl;
-      read_spec := read_impl_spec;
-    |}.
-    
-  
-End impl.
 
 (*
 Arguments newBag {_ _} _.
