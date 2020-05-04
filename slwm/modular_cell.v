fde2aa00e6f6f15a4dcd4fbd2c6c67f642200064
Abel Nieto <abeln@cs.au.dk>
2020-05-04 10:45:41 +0200

    copy code and old proof

--- slwm/modular_cell.v
+++ slwm/modular_cell.v
@@ -17,6 +17,8 @@ Definition cntΣ : gFunctors := #[GFunctor cntCmra ].
 Instance subG_cntΣ {Σ} : subG cntΣ Σ → cntG Σ.
 Proof. solve_inG. Qed.
 
+Context `{!heapG Σ, !cntG Σ, spawnG Σ} (N : namespace).
+
 Section cell_impl.
   
   Definition new_cell : val := λ: "m", ref "m".
@@ -26,8 +28,6 @@ Section cell_impl.
   Definition write : val := λ: "l" "w", "l" <- "w". 
   
 End cell_impl.
-
-
 
 Section cell_model.
   Context `{!cntG Σ}.
@@ -109,8 +109,6 @@ Notation "γ ⤇½ m" := (own γ (makeElem (1/2) m))
   (at level 20, format "γ ⤇½  m") : bi_scope.
 
 Section cell_impl.
-  Context `{!heapG Σ, !cntG Σ, spawnG Σ} (N : namespace).
-
   Definition cell_inv ℓ γ := (∃ (m : Z), ℓ ↦ #m ∗ γ ⤇½ m)%I.
 
   Definition Cell (ℓ : loc) (γ : gname) : iProp Σ :=
@@ -264,6 +262,76 @@ Section cell_impl.
       iNext. iApply "Hcont". auto.
   Qed.
 
+  (* TODO: prove code from SLWM paper *)
         
 End cell_impl.
+
+Section mp_code.
+
+  (* First we have a function `repeat l`, which reads l until its value is non-zero,
+     at which point it returns l's value. *)
+  Definition repeat_prog : val :=
+    rec: "repeat" "l" :=
+      let: "vl" := read "l" in
+      if: "vl" = #0 then ("repeat" "l") else "vl".
+
+  (* Then we have the code for the example. *)
+  Definition mp : val :=
+    λ: <>,
+       let: "x" := new_cell #0 in
+       let: "y" := new_cell #0 in
+       let: "res" := ((write "x" #37;; write "y" #1) ||| (repeat_prog "y";; !"x")) in
+       Snd "res".
+
+End mp_code.
+
+Section mp_spec.
+  
+  Lemma mp_spec :
+    {{{ True }}} mp #() {{{ v, RET #v ; ⌜v = 37⌝ }}}.
+  Proof.
+    iIntros (Φ) "_ HPost".
+    rewrite /mp. wp_pures.
+    wp_alloc lx as "Hlx". wp_pures. wp_alloc ly as "Hly". wp_pures.
+    wp_bind (par _ _).
+    iMod (own_alloc (Excl ())) as (γ) "Hown".
+    { constructor. }    
+    iMod (inv_alloc (invN "outer") _ (inv_out ly lx γ) with "[Hly]") as "#Hinv".
+    { iNext. rewrite /inv_out. iLeft. iFrame. }
+    wp_apply (wp_par (λ _, True)%I (λ vx, ⌜vx = #37⌝)%I with "[Hinv Hlx] [Hinv Hown]").
+    - wp_store.
+      iMod (inv_alloc (invN "inner") _ (inv_in lx γ) with "[Hlx]") as "#Hinv_in".
+      { iNext. iLeft. iFrame. }
+      iInv (invN "outer") as "[Hy0 | [Hy1 _]]" "Hclose"; wp_store.
+      * iMod ("Hclose" with "[Hinv_in Hy0]") as "_".        
+        iNext; iRight; iFrame; iFrame "#". by iModIntro.
+      * iMod ("Hclose" with "[Hinv_in Hy1]") as "_".        
+        iNext; iRight; iFrame; iFrame "#". by iModIntro.
+    - wp_bind (repeat_prog _).
+      iLöb as "IH".
+      rewrite /repeat_prog. wp_pures. wp_bind (!_)%E.
+      iInv (invN "outer") as "[Hy0 | [Hy1 #Hinv_in]]" "Hclose".
+      * wp_load.
+        iMod ("Hclose" with "[Hy0]") as "_".
+        { iLeft. iNext. iFrame. }
+        iModIntro.
+        wp_let. wp_pure _. wp_if.
+        iApply "IH". iFrame.
+      * wp_load.
+        iInv (invN "inner") as "[Hlx | Hown2]" "Hclose_in".        
+        + iMod ("Hclose_in" with "[Hown]") as "_".
+          { iRight. iNext; iFrame. }
+          iMod ("Hclose" with "[Hy1]") as "_".
+          { iRight. iNext. by iFrame; iFrame "#". }
+          iModIntro. wp_pures. wp_load. iPureIntro; auto.
+        +  iDestruct "Hown2" as "> Hown2". iExFalso.
+           iDestruct (own_valid_2 with "Hown Hown2") as %Hv. done.
+    - iIntros (v1 v2) "[_ ->]".
+      iNext. wp_pures. iApply "HPost". iPureIntro. reflexivity.
+  Qed.
+      
+           
+  
+  (* Global Opaque newlock release acquire. *)
+End mp_spec.
 
